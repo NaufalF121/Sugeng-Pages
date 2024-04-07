a2ca608ca7e316f68af60c195fafc9565295c5c6
NaufalF121 <blueshoko@gmail.com>
2024-04-08 03:38:15 +0700

    hide scroll bar

--- app.vue
+++ app.vue
@@ -1,4 +1,15 @@
-
+<style>
+  ::-webkit-scrollbar {
+    display: none;
+  }
+  body::-webkit-scrollbar {
+    display: none;
+  }
+  body {
+    -ms-overflow-style: none;  /* IE and Edge */
+    scrollbar-width: none;  /* Firefox */
+  }
+</style>
 <template>
   <div class="flex flex-col bg-yamada justify-center items-center gap-2">
     <Header />
