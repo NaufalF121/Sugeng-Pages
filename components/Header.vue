0a98dc316f94f89e167b26a4222cb3227868578c
NaufalF121 <blueshoko@gmail.com>
2024-04-08 02:04:29 +0700

    init

---
+++ components/Header.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="flex flex-row w-full h-14 sticky top-0 bg-transparent items-center pl-5 pr-5">
+        <p class="text-black font-mono">Sugeng.cpp</p>
+    </div>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+      // Configure your color palette here
+      'nijika': '#E6D47B',
+      'yamada': '#4061A0',
+      'hitori': '#EEB8C4',
+      'ikuyo': '#D0574E',
+    }
+  },
+    
+  },
+  plugins: [],
+}
+
