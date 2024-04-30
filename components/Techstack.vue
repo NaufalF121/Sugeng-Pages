da6b58499b03be75c6ae31d8032a518a77483859
NaufalF121 <blueshoko@gmail.com>
2024-04-30 13:04:44 +0700

    update tech stack

--- components/Techstack.vue
+++ components/Techstack.vue
@@ -2,7 +2,7 @@
     <section id="tech" class="w-full h-dhv">
         <div class="flex flex-col w-full h-dvh bg-slate-200 justify-center items-center gap-5">
             <h1 class="text-6xl font-mono font-bold">Techstacks</h1>
-            <img/>
+            <img src="/Language.png" width="1024" height="1024"/>
         </div>
     </section>
 
