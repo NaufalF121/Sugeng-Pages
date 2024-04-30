1a461f11aee07daece4bcd11e932de4e77c6e241
NaufalF121 <blueshoko@gmail.com>
2024-04-30 13:01:42 +0700

    update about and techstack

--- components/About.vue
+++ components/About.vue
@@ -1,10 +1,10 @@
 <template>
     <section class="w-full h-dhv " id="about">
         <div class="flex flex-row w-full h-dvh bg-slate-200 justify-center items-center static ">
-            <img src="/profile.png" alt="Description of image" width="900" height="900" class="z-20" />
+            <img src="/profile.png" alt="Description of image" width="869" height="869" class="z-20" />
             <div class="flex flex-col justify-center gap-5 pr-10">
-                <h1 class="text-6xl font-mono font-bold">About</h1>
-                <p class="text-xl font-mono text-balance">I am a fullstack developer, now iam currently studying computer science at
+                <!-- <h1 class="text-6xl font-mono font-bold">About</h1> -->
+                <p class="text-xl font-mono text-balance">My real name is Naufal Farras Pratama, now iam currently studying computer science at
                     State University Surabaya. In another hand i have interest in artificial intelligence and data field
                     , especially computer vision. I am capable to
                     collaborating with others in a team and thrive in fast-paced environments.</p>
