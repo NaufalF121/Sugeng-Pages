3b97520d906c0cdddacf99ac5fe8e2fd6b0acdde
NaufalF121 <blueshoko@gmail.com>
2024-05-03 13:26:05 +0700

    update layout

--- components/About.vue
+++ components/About.vue
@@ -1,8 +1,8 @@
 <template>
     <section class="w-full h-dhv " id="about">
         <div class="flex flex-row w-full h-dvh bg-slate-200 justify-center items-center static ">
-            <img src="/profile.png" alt="Description of image" width="869" height="869" class="z-20" />
-            <div class="flex flex-col justify-center gap-5 pr-10">
+            <img src="/profile.png" alt="Description of image" width="869" height="869" class="z-20 pl-5" />
+            <div class="flex flex-col justify-center gap-5 w-1/3">
                 <!-- <h1 class="text-6xl font-mono font-bold">About</h1> -->
                 <p class="text-xl font-mono text-balance">My real name is Naufal Farras Pratama, now iam currently studying computer science at
                     State University Surabaya. In another hand i have interest in artificial intelligence and data field
