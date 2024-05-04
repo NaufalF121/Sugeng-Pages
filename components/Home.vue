b0e14c5d2bc982a5adbdd6666e23c84277184b25
NaufalF121 <blueshoko@gmail.com>
2024-05-04 17:52:09 +0700

    animate border

--- components/Home.vue
+++ components/Home.vue
@@ -57,23 +57,27 @@ export default {
 <template>
     <section id="home">
         <div class="bg-slate-200 relative w-full h-dvh">
-            <div class="absolute inset-x-64 top-40">
+            <div class="absolute inset-x-64 top-40 ">
                 <!-- <div class="w-64 bg-slate-200 h-64 rounded-full bg-yamada-light blur-3xl brightness-100 z-0"></div> -->
-                <div class="w-32 h-32 rounded-full bg-gradient-to-bl from-yamada-light to-nijika z-0 p-1">
-                    <img src="/about.png" class="w-full h-full rounded-full" />
+                <div class="w-32 h-32 rounded-full bg-gradient-to-bl from-yamada-light to-nijika z-0 p-1 animate-spin">
+                    <div class="w-full h-full rounded-full bg-gradient-to-bl from-yamada-light to-nijika"></div>
                 </div>
             </div>
+            <div class="p-1 absolute z-10 inset-x-64 top-40 w-32 h-32 rounded-full">
+                <img src="/about.png" class="w-full h-full rounded-full " />
+            </div>
+
             <div class="flex flex-col w-full h-dvh justify-center items-center gap-5 static">
                 <h1 class="text-6xl font-mono font-bold">Sugeng.cpp here !🍀</h1>
                 <h2 id="name" class="text-3xl font-mono ">Aka </h2>
                 <div class="absolute bottom-20 flex flex-row">
-                <a class="text-black">Swipe down</a>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"
-                    class="h-6 w-6 ml-2">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                        d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
-                </svg>
-            </div>
+                    <a class="text-black">Swipe down</a>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"
+                        class="h-6 w-6 ml-2">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
+                    </svg>
+                </div>
             </div>
             <!-- <div class="w-64 h-64 rounded-md opacity-30 bg-gray-800 absolute bottom-10 left-40 rotate-6 flex flex-row items-center justify-center">
                 <a class="text-3xl font-mono font-bold">
@@ -83,7 +87,7 @@ export default {
             <div class="w-64 h-64 rounded-md opacity-10 bg-gray-700 absolute bottom-10 left-40 "></div>
             <a class="absolute bottom-60 right-20 -rotate-6 font-mono font-bolf text-3xl text-slate-300 opacity-30">include bits/stdc++.h</a> -->
 
-            
+
         </div>
 
     </section>
