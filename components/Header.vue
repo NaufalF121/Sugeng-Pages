b31424b31b96f168b019b6c7b11ffed28752b903
NaufalF121 <blueshoko@gmail.com>
2024-05-04 19:29:56 +0700

    update header and techstack

--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex flex-row w-full h-14 sticky top-0 bg-transparent items-center pl-5 justify-center pr-5 gap-10">
+    <div class="flex flex-row w-full h-14 sticky top-0 bg-transparent items-center pl-5 justify-center pr-5 gap-10 z-50">
         <div class="pr-2 pl-2 rounded-md bg-transparent hover:opacity-50">
             <a href="#home" class="text-black font-mono">Home</a>
         </div>
@@ -12,8 +12,6 @@
         <div class="pr-2 pl-2 rounded-md bg-transparent hover:opacity-50">
             <a href="#exp" class="text-black font-mono">Experience</a>
         </div>
-        
-        
         
     </div>
 </template>
--- components/Techstack.vue
+++ components/Techstack.vue
@@ -1,7 +1,10 @@
 <template>
     <section id="tech" class="w-full h-dhv">
-        <div class="flex flex-col w-full h-dvh bg-slate-200 justify-center items-center pt-5">
-            <img src="/Language.png" width="1280" height="720"/>
+        <div class="flex flex-col w-full h-dvh bg-slate-200 justify-center items-center pt-5 relative">
+            <img src="/about.png" class="w-32 h-32 rounded-md border-8 border-gray-100 absolute inset-x-72 top-12" />
+            <div class="bg-gray-100 w-3/4 h-3/4 px-5 pt-16">
+                
+            </div>
         </div>
     </section>
 
