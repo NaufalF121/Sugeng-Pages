8966d93a2f88c66095d5c225fb70eeaeff1685e9
NaufalF121 <blueshoko@gmail.com>
2024-05-04 17:05:27 +0700

    update profile

--- components/Techstack.vue
+++ components/Techstack.vue
@@ -1,8 +1,7 @@
 <template>
     <section id="tech" class="w-full h-dhv">
-        <div class="flex flex-col w-full h-dvh bg-slate-200 justify-center items-center gap-5">
-            <h1 class="text-6xl font-mono font-bold">Techstacks</h1>
-            <img src="/Language.png" width="1024" height="1024"/>
+        <div class="flex flex-col w-full h-dvh bg-slate-200 justify-center items-center pt-5">
+            <img src="/Language.png" width="1280" height="720"/>
         </div>
     </section>
 
