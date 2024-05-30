32c0110bee31125f9375da995191a6a6c7997694
NaufalF121 <blueshoko@gmail.com>
2024-05-30 22:02:45 +0700

    finisihing techstack

--- components/Experiance.vue
+++ components/Experiance.vue
@@ -1,7 +1,7 @@
 <template>
     <section id="exp" class="w-full h-dhv">
         <div class="flex flex-col w-full h-dvh bg-ikuyo justify-center items-center gap-5">
-            <h1 class="text-6xl font-mono font-bold">Experience</h1>
+            <h1 class="text-2xl font-mono font-bold">Experience still under construction 🛠</h1>
         </div>
     </section>
 
