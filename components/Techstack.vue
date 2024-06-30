de1040af95e3851f2670d51d6b66b44b8d018003
NaufalF121 <blueshoko@gmail.com>
2024-06-30 20:21:08 +0700

    refactor: add swiper

--- components/Techstack.vue
+++ components/Techstack.vue
@@ -1,60 +1,109 @@
-
-
 <script lang="ts">
-import { Swiper, SwiperSlide } from 'swiper/vue';
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
-import 'swiper/css/navigation';
-  import 'swiper/css/pagination';
-  import 'swiper/css/scrollbar';
+import { Swiper, SwiperSlide } from "swiper/vue";
+import { Pagination, Autoplay } from "swiper/modules";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 // Import Swiper styles
-import 'swiper/css';
+import "swiper/css";
+
 export default {
-    components: {
-      Swiper,
-      SwiperSlide,
-    },setup() {
-      
-      return {
-      
-        modules: [Pagination],
-      };
-    },
-}
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  setup() {
+    return {
+      Title: "<Techstack/>",
+
+      modules: [Pagination, Autoplay],
+    };
+  },
+};
 </script>
 
-
 <template>
-    <section id="tech" class="w-full h-dhv">
-        <div class="flex flex-col w-full bg-slate-200 justify-center items-center relative">
-            <!-- <img src="/about.png" class="w-32 h-32 rounded-md border-8 border-gray-100 absolute inset-x-72 top-12 z-10" /> -->
-            <swiper
-            class="px-3 py-4 w-64 flex justify-center items-center"
-            :modules="modules" 
-            :pagination="{ clickable: true }"
-    
-  >
-    <swiper-slide>
-        <div class="w-64 h-64 bg-orange-600">Programming language</div>
-    </swiper-slide>
-    <swiper-slide>
-        <div class="w-64 h-64 bg-orange-600">Backend</div>
-    </swiper-slide>
-    <swiper-slide>
-        <div class="w-64 h-64 bg-orange-600">AI dan Data</div>
-    </swiper-slide>
-    <swiper-slide>
-        <div class="w-64 h-64 bg-orange-600">Frontend</div>
-    </swiper-slide>
-    ...
-  </swiper>
-            <div
-                class="bg-gray-100 bg-clip-padding backdrop-filter backdrop-blur-3xl bg-opacity-30 border border-gray-100 w-3/4 max-h-fit px-5 py-16 flex flex-col m-2 justify-center relative items-center text-center z-0 rounded-lg">
-                <h1 class="text-2xl font-mono text-balance font-medium"> Techstacks</h1>
-                <svg height="10" width="500">
-                    <line x1="175" y1="0" x2="325" y2="0" style="stroke:rgb(0,0,0);stroke-width:5" />
-                </svg>
+  <section id="tech" class="w-full h-dhv">
+    <div
+      class="flex flex-col w-full bg-slate-200 justify-center items-center relative"
+    >
+      <!-- <img src="/about.png" class="w-32 h-32 rounded-md border-8 border-gray-100 absolute inset-x-72 top-12 z-10" /> -->
 
-                <div class="w-full mt-10 grid grid-cols-5 row-auto
+      <div
+        class="bg-gray-100 bg-clip-padding backdrop-filter backdrop-blur-3xl bg-opacity-30 border border-gray-100 w-3/4 max-h-fit px-5 py-5 flex flex-col m-2 justify-center relative items-center text-center z-0 rounded-lg"
+      >
+        <h1 class="text-2xl font-mono text-balance font-medium">{{ Title }}</h1>
+        <svg height="10" width="500">
+          <line
+            x1="175"
+            y1="0"
+            x2="325"
+            y2="0"
+            style="stroke: rgb(0, 0, 0); stroke-width: 5"
+          />
+        </svg>
+        <swiper
+          class="px-3 py-4 w-full"
+          :modules="modules"
+          :pagination="{ clickable: true }"
+          :autoplay="{ delay: 2000, pauseOnMouseEnter: true }"
+          :spaceBetween="30"
+          :loop="true"
+          :centeredSlides="true"
+          :centeredSlidesBounds="true"
+        >
+          <swiper-slide>
+            <div
+              class="bg-slate-200 w-full p-4 flex justify-center items-center"
+            >
+              <div class="w-full m-5 h-64 border-5">
+                <a class="text-xl">Programming language</a>
+                <div
+                  class="grid grid-cols-5 gap-5 mt-5 h-full items-center place-content-center"
+                >
+                  <div class="justify-center items-center">
+                    <Icon name="fa6-brands:golang" color="#00ACD7" size="100" />
+                  </div>
+                  <div class="justify-center items-center">
+                    <Icon name="logos:python" size="100" />
+                  </div>
+                  <div class="justify-center items-center">
+                    <Icon name="devicon:typescript" size="100" />
+                  </div>
+                  <div class="justify-center items-center">
+                    <Icon name="logos:postgresql" size="100" />
+                  </div>
+                  <div class="justify-center items-center">
+                    <Icon name="logos:nodejs" size="100" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </swiper-slide>
+          <swiper-slide>
+            <div
+              class="bg-slate-200 w-full p-4 flex justify-center items-center"
+            >
+              <div class="w-96 h-64 border-5">Backend</div>
+            </div>
+          </swiper-slide>
+          <swiper-slide>
+            <div
+              class="bg-slate-200 w-full p-4 flex justify-center items-center"
+            >
+              <div class="w-96 h-64 border-5">Frontend</div>
+            </div>
+          </swiper-slide>
+          <swiper-slide>
+            <div
+              class="bg-slate-200 w-full p-4 flex justify-center items-center"
+            >
+              <div class="w-96 h-64 border-5">AI & Data</div>
+            </div>
+          </swiper-slide>
+          ...
+        </swiper>
+        <!-- <div class="w-full mt-10 grid grid-cols-5 row-auto
                     px-2 md:columns-3 gap-5
                     md:px-5 lg:columns-4 xl:columns-5 xl:gap-10">
                     <div class="justify-center items-center">
@@ -70,70 +119,98 @@ export default {
                         <Icon name="logos:postgresql" size="100" />
                     </div>
                     <div class="flex justify-center items-center">
-                        <div><svg width="100" height="100" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                d="M94.7304 29.3213C94.7297 27.6444 94.371 26.1626 93.6457 24.8908C92.9333 23.6402 91.8664 22.5921 90.4353 21.7633C78.6222 14.9519 66.7977 8.16125 54.9885 1.34284C51.8048 -0.495164 48.7179 -0.428157 45.5578 1.43624C40.8559 4.20922 17.3148 17.6964 10.2999 21.7595C7.41097 23.4319 6.00521 25.9914 6.00451 29.3181C6 43.0164 6.00451 56.7143 6 70.4129C6.00069 72.0531 6.34406 73.506 7.0374 74.7587C7.75017 76.0471 8.83201 77.1241 10.2961 77.9716C17.3114 82.0347 40.8555 95.5208 45.5565 98.2945C48.7179 100.16 51.8048 100.227 54.9895 98.3879C66.7991 91.5691 78.6243 84.7789 90.4391 77.9674C91.9032 77.1203 92.985 76.0426 93.6978 74.7552C94.3901 73.5026 94.7342 72.0496 94.7349 70.4091C94.7349 70.4091 94.7349 43.0199 94.7304 29.3213"
-                                fill="#5C8DBC" />
-                            <path
-                                d="M50.5032 49.7322L7.03735 74.7588C7.75013 76.0473 8.83197 77.1242 10.2961 77.9717C17.3113 82.0349 40.8555 95.521 45.5565 98.2947C48.7179 100.16 51.8048 100.227 54.9895 98.3881C66.7991 91.5693 78.6244 84.779 90.4392 77.9676C91.9033 77.1204 92.9851 76.0427 93.6979 74.7554L50.5032 49.7322"
-                                fill="#1A4674" />
-                            <path
-                                d="M37.629 57.1453C40.1583 61.5608 44.913 64.5389 50.3673 64.5389C55.8553 64.5389 60.6371 61.5229 63.1536 57.0602L50.5031 49.7322L37.629 57.1453"
-                                fill="#1A4674" />
-                            <path
-                                d="M94.7303 29.3216C94.7296 27.6447 94.371 26.1629 93.6457 24.8911L50.5032 49.7324L93.6978 74.7556C94.3901 73.503 94.7342 72.05 94.7349 70.4095C94.7349 70.4095 94.7349 43.0202 94.7303 29.3216"
-                                fill="#1B598E" />
-                            <path
-                                d="M92.3551 51.5208H88.9798V54.8969H85.6037V51.5208H82.2287V48.1459H85.6037V44.7705H88.9798V48.1459H92.3551V51.5208ZM80.0387 51.5208H76.6637V54.8969H73.2883V51.5208H69.913V48.1459H73.2883V44.7705H76.6637V48.1459H80.0387V51.5208Z"
-                                fill="white" />
-                            <path
-                                d="M63.1537 57.0604C60.6373 61.5231 55.8555 64.5392 50.3675 64.5392C44.9131 64.5392 40.1584 61.561 37.6291 57.1455C36.3586 54.9298 35.6913 52.4197 35.6939 49.8656C35.6939 41.7619 42.2637 35.1924 50.3675 35.1924C55.7867 35.1924 60.5164 38.1335 63.0582 42.5039L75.8837 35.1185C70.7859 26.3187 61.2685 20.3984 50.3675 20.3984C34.0927 20.3984 20.8999 33.5916 20.8999 49.8656C20.8999 55.2047 22.3202 60.2115 24.8026 64.5301C29.8875 73.3765 39.4321 79.3332 50.3675 79.3332C61.323 79.3332 70.8824 73.3525 75.96 64.4801L63.1537 57.0604Z"
-                                fill="white" />
-                        </svg></div>
+                        <div><svg width="100" height="100" viewBox="0 0 100 100" fill="none"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M94.7304 29.3213C94.7297 27.6444 94.371 26.1626 93.6457 24.8908C92.9333 23.6402 91.8664 22.5921 90.4353 21.7633C78.6222 14.9519 66.7977 8.16125 54.9885 1.34284C51.8048 -0.495164 48.7179 -0.428157 45.5578 1.43624C40.8559 4.20922 17.3148 17.6964 10.2999 21.7595C7.41097 23.4319 6.00521 25.9914 6.00451 29.3181C6 43.0164 6.00451 56.7143 6 70.4129C6.00069 72.0531 6.34406 73.506 7.0374 74.7587C7.75017 76.0471 8.83201 77.1241 10.2961 77.9716C17.3114 82.0347 40.8555 95.5208 45.5565 98.2945C48.7179 100.16 51.8048 100.227 54.9895 98.3879C66.7991 91.5691 78.6243 84.7789 90.4391 77.9674C91.9032 77.1203 92.985 76.0426 93.6978 74.7552C94.3901 73.5026 94.7342 72.0496 94.7349 70.4091C94.7349 70.4091 94.7349 43.0199 94.7304 29.3213"
+                                    fill="#5C8DBC" />
+                                <path
+                                    d="M50.5032 49.7322L7.03735 74.7588C7.75013 76.0473 8.83197 77.1242 10.2961 77.9717C17.3113 82.0349 40.8555 95.521 45.5565 98.2947C48.7179 100.16 51.8048 100.227 54.9895 98.3881C66.7991 91.5693 78.6244 84.779 90.4392 77.9676C91.9033 77.1204 92.9851 76.0427 93.6979 74.7554L50.5032 49.7322"
+                                    fill="#1A4674" />
+                                <path
+                                    d="M37.629 57.1453C40.1583 61.5608 44.913 64.5389 50.3673 64.5389C55.8553 64.5389 60.6371 61.5229 63.1536 57.0602L50.5031 49.7322L37.629 57.1453"
+                                    fill="#1A4674" />
+                                <path
+                                    d="M94.7303 29.3216C94.7296 27.6447 94.371 26.1629 93.6457 24.8911L50.5032 49.7324L93.6978 74.7556C94.3901 73.503 94.7342 72.05 94.7349 70.4095C94.7349 70.4095 94.7349 43.0202 94.7303 29.3216"
+                                    fill="#1B598E" />
+                                <path
+                                    d="M92.3551 51.5208H88.9798V54.8969H85.6037V51.5208H82.2287V48.1459H85.6037V44.7705H88.9798V48.1459H92.3551V51.5208ZM80.0387 51.5208H76.6637V54.8969H73.2883V51.5208H69.913V48.1459H73.2883V44.7705H76.6637V48.1459H80.0387V51.5208Z"
+                                    fill="white" />
+                                <path
+                                    d="M63.1537 57.0604C60.6373 61.5231 55.8555 64.5392 50.3675 64.5392C44.9131 64.5392 40.1584 61.561 37.6291 57.1455C36.3586 54.9298 35.6913 52.4197 35.6939 49.8656C35.6939 41.7619 42.2637 35.1924 50.3675 35.1924C55.7867 35.1924 60.5164 38.1335 63.0582 42.5039L75.8837 35.1185C70.7859 26.3187 61.2685 20.3984 50.3675 20.3984C34.0927 20.3984 20.8999 33.5916 20.8999 49.8656C20.8999 55.2047 22.3202 60.2115 24.8026 64.5301C29.8875 73.3765 39.4321 79.3332 50.3675 79.3332C61.323 79.3332 70.8824 73.3525 75.96 64.4801L63.1537 57.0604Z"
+                                    fill="white" />
+                            </svg></div>
                     </div>
                     <div class="justify-center items-center">
                         <Icon name="vscode-icons:file-type-mongo" size="100" />
                     </div>
-                    <div class="justify-center items-center"><Icon name="uil:react" size="100" /></div>
-                    <div class="justify-center items-center" ><Icon name="logos:nodejs" size="100" /></div>
-                    <div class="justify-center items-center"><Icon name="logos:vue" size="100" /></div>
-                    <div class="justify-center items-center"><Icon name="logos:pytorch-icon" size="100"/></div>
-                    <div class="justify-center items-center"><Icon name="logos:tailwindcss-icon" size="100"/></div>
-                    <div class="justify-center items-center"><Icon name="logos:docker-icon" size="100"/></div>
-                    <div class="justify-center items-center"><Icon name="logos:linux-tux" size="100"/></div>
-                    <div class="justify-center items-center"><Icon name="logos:github-icon" size="100"/></div>
-                    <div class="justify-center items-center"><Icon name="logos:pandas-icon" size="100"/></div>
-                    <div class="justify-center items-center"><Icon name="logos:numpy" size="100"/></div>
-                    <div class="justify-center items-center"><Icon name="logos:nextjs-icon" size="100"/></div>
-                    <div class="justify-center items-center"><Icon name="logos:aws-s3" size="100"/></div>
-                    <div class="justify-center items-center"><Icon name="logos:git-icon" size="100"/></div>
-                    <div class="justify-center items-center"><Icon name="logos:azure-icon" size="100"/></div>
+                    <div class="justify-center items-center">
+                        <Icon name="uil:react" size="100" />
+                    </div>
+                    <div class="justify-center items-center">
+                        <Icon name="logos:nodejs" size="100" />
+                    </div>
+                    <div class="justify-center items-center">
+                        <Icon name="logos:vue" size="100" />
+                    </div>
+                    <div class="justify-center items-center">
+                        <Icon name="logos:pytorch-icon" size="100" />
+                    </div>
+                    <div class="justify-center items-center">
+                        <Icon name="logos:tailwindcss-icon" size="100" />
+                    </div>
+                    <div class="justify-center items-center">
+                        <Icon name="logos:docker-icon" size="100" />
+                    </div>
+                    <div class="justify-center items-center">
+                        <Icon name="logos:linux-tux" size="100" />
+                    </div>
+                    <div class="justify-center items-center">
+                        <Icon name="logos:github-icon" size="100" />
+                    </div>
+                    <div class="justify-center items-center">
+                        <Icon name="logos:pandas-icon" size="100" />
+                    </div>
+                    <div class="justify-center items-center">
+                        <Icon name="logos:numpy" size="100" />
+                    </div>
+                    <div class="justify-center items-center">
+                        <Icon name="logos:nextjs-icon" size="100" />
+                    </div>
+                    <div class="justify-center items-center">
+                        <Icon name="logos:aws-s3" size="100" />
+                    </div>
+                    <div class="justify-center items-center">
+                        <Icon name="logos:git-icon" size="100" />
+                    </div>
+                    <div class="justify-center items-center">
+                        <Icon name="logos:azure-icon" size="100" />
+                    </div>
 
 
-                </div>
-
-            </div>
-        </div>
-    </section>
+                </div> -->
+      </div>
+    </div>
+  </section>
 </template>
 <style>
 .cls-1 {
-    fill: #130754;
+  fill: #130754;
 }
 
 .cls-2 {
-    fill: #ffca00;
+  fill: #ffca00;
 }
 
 .cls-3 {
-    fill: #e70488;
+  fill: #e70488;
 }
 
 .cls-4 {
-    fill: #4d77cf;
+  fill: #4d77cf;
 }
 
 .cls-5 {
-    fill: #4dabcf;
-}</style>
-
+  fill: #4dabcf;
+}
+</style>
