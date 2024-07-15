584b829c21d886f6fb6f83573d86925ce9778681
NaufalF121 <blueshoko@gmail.com>
2024-07-15 22:07:41 +0700

    fix: responsive blog page

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -3,32 +3,30 @@ const { slug } = useRoute().params;
 </script>
 
 <template>
-  
-  <article class="bg-transparent rounded-2xl h-dvh">
-    <ContentDoc :path="`/blog/${slug}`" v-slot="{ doc }">
-      <!-- Header  -->
-      <header>
-        <div class="text-center p-5">
-          <h1 class="text-4xl font-bold lg:w-2/3 mx-auto">{{ doc.title }}</h1>
-          <p class="text-gray-500 text-sm mt-2">{{ doc.date }}</p>
+  <article class="bg-inherit rounded-2xl min-h-dvh w-full flex justify-center">
+    <div class="w-full flex flex-col items-center">
+      <ContentDoc :path="`/blog/${slug}`" v-slot="{ doc }" class="flex flex-col items-center w-full">
+       
+        <div class="flex flex-col items-center w-full">
+          <div class="text-center p-5">
+            <h1 class="text-4xl font-bold lg:w-2/3 mx-auto">{{ doc.title }}</h1>
+            <p class="text-gray-500 text-sm mt-2">{{ doc.date }}</p>
+          </div>
+          <NuxtImg
+            v-if="doc.thumb"
+            :src="doc.thumb"
+            :placeholder="[128, 128, 10, 5]"
+            :alt="doc.title"
+            sizes="69vw md:50vw"
+            class="object-cover rounded-2xl"
+          />
         </div>
-        <img
-          v-if="doc.thumb"
-          :src="doc.thumb"
-          :alt="doc.title"
-          class="w-full"
-        />
-      </header>
-      <!-- ./ Header  -->
-
-      <!-- Content -->
-      <div class="mt-4 content p-5">
-        <!-- Content  -->
-        <ContentRenderer :value="doc" />
-        <!-- ./ Content  -->
-      </div>
-      <!-- ./ Content  -->
-    </ContentDoc>
+     
+        <div class="w-full flex justify-center mt-5">
+          <ContentRenderer :value="doc" class="w-2/3 text-justify"/>
+        </div>
+      </ContentDoc>
+    </div>
   </article>
 </template>
 
@@ -44,7 +42,6 @@ const { slug } = useRoute().params;
 .content table:not(:last-child) {
   @apply mb-4;
 }
-
 .content h1 {
   @apply text-3xl font-bold;
 }
