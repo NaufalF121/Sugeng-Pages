f35ef7caa2adda86c7f815d7450de82bc167894a
elskow <elskow@outlook.com>
2024-09-14 19:21:17 +0700

    ui: fixing not centerized issue of the home/hero section

--- components/Home.vue
+++ components/Home.vue
@@ -4,7 +4,7 @@ const colors = ["bg-clip-text text-transparent bg-gradient-to-bl from-yamada-lig
 colorIndex = 0;
 export default {
     mounted() {
-        
+
         new TypeIt('#interest', {
             speed: 100,
             waitUntilVisible: true,
@@ -21,7 +21,7 @@ export default {
 
 <template>
     <section id="home">
-        <div class="bg-slate-200 w-full h-dvh relative gap-10 justify-center items-center flex flex-col">
+        <div class="bg-slate-200 w-full h-dvh relative gap-10 justify-center items-center flex flex-col pb-36">
           <div class="relative w-32 h-32">
             <div class="absolute ">
                 <!-- <div class="w-64 bg-slate-200 h-64 rounded-full bg-yamada-light blur-3xl brightness-100 z-0"></div> -->
@@ -37,16 +37,16 @@ export default {
             <div class="flex flex-col w-full justify-center items-center gap-5 static">
                 <h1 class="lg:text-6xl text-3xl font-mono font-bold">Sugeng.cpp here !🍀</h1>
                 <h2 class="lg:text-3xl text-lg font-mono " id="interest">Interested in </h2>
-                <div class="cursor-pointer rounded-lg border-2 border-gray-800 p-1 hover:text-gray-100 hover:bg-gray-800"> 
+                <div class="cursor-pointer rounded-lg border-2 border-gray-800 p-1 hover:text-gray-100 hover:bg-gray-800">
                     <a class="font-mono font-bold" href="mailto:blueshoko@gmail.com">Contact Me</a>
                 </div>
-                
+
                 <div class="absolute bottom-20 flex flex-row">
                     <a class="text-black">Swipe down</a>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"
-                        class="h-6 w-6 ml-2">
+                         class="h-6 w-6 ml-2 animate-bounce">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
+                              d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
                     </svg>
                 </div>
             </div>
