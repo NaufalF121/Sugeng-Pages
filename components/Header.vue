090aa375ce47dbbe858d7895c35178950d8dfe5c
NaufalF121 <blueshoko@gmail.com>
2026-05-25 09:06:00 +0700

    Add header icons and update footer year
    Regenerate package-lock.json (peer flags updated)

--- components/Header.vue
+++ components/Header.vue
@@ -1,18 +1,30 @@
 <template>
     <div class="flex flex-row w-full h-14 top-0 bg-transparent items-center pl-5 justify-center pr-5 lg:gap-10 gap-5 z-50">
         <div class="pr-2 pl-2 rounded-md bg-transparent hover:bg-gray-500">
-            <NuxtLink to="/" class="text-black font-mono">Home</NuxtLink>
+            <NuxtLink to="/" class="text-black font-mono" aria-label="Home" title="Home">
+                <Icon name="mdi:home" :size="iconSize" />
+                <span class="sr-only">Home</span>
+            </NuxtLink>
         </div>
         <div class="pr-2 pl-2 rounded-md bg-transparent hover:bg-gray-500">
-            <NuxtLink to="/blog" class="text-black font-mono">Blog</NuxtLink>
+            <NuxtLink to="/blog" class="text-black font-mono" aria-label="Blog" title="Blog">
+                <Icon name="mdi:post-outline" :size="iconSize" />
+                <span class="sr-only">Blog</span>
+            </NuxtLink>
         </div>
         <div class="pr-2 pl-2 rounded-md bg-transparent hover:bg-gray-500">
-            <NuxtLink to="/project" class="text-black font-mono">Projects</NuxtLink>
+            <NuxtLink to="/project" class="text-black font-mono" aria-label="Projects" title="Projects">
+                <Icon name="mdi:briefcase-outline" :size="iconSize" />
+                <span class="sr-only">Projects</span>
+            </NuxtLink>
         </div>
         <div class="pr-2 pl-2 rounded-md bg-transparent hover:bg-gray-500">
-            <NuxtLink to="/about" class="text-black font-mono">More</NuxtLink>
+            <NuxtLink to="/about" class="text-black font-mono" aria-label="More" title="More">
+                <Icon name="mdi:dots-horizontal-circle-outline" :size="iconSize" />
+                <span class="sr-only">More</span>
+            </NuxtLink>
         </div>
-    
+
     </div>
 </template>
 
@@ -20,7 +32,7 @@
 export default {
     data() {
         return {
-            iconSize: 48,
+            iconSize: 22,
         };
     },
 };
